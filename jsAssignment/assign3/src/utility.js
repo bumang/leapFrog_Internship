a81fb97f9060c7e4d3929b6607464e7449f60ca6
bumang <umangabhattarai11@gmail.com>
2021-12-28 07:31:21 +0545

    dev:atomic push ball collision still left

---
+++ jsAssignment/assign3/src/utility.js
@@ -0,0 +1,33 @@
+/* generating random numbers for position */
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+/* generating random color */
+
+function generateRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+/* generating random direction */
+
+function getDirection() {
+    return Math.random() > 0.5 ? 1 : -1;
+}
+
+/* get distance between the objects using pythagoras theorem */
+
+function getDistance(x1, y1, x2, y2) {
+    let xDistance = x2 - x1;
+    let yDistance = y2 - y1;
+
+    return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+
+}
